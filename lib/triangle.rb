4d3c98ce5706cad07bdbdcb7d7aefd2329758270
github username <dreedsanders@gmail.com>
2021-03-03 17:16:27 -0600

    commit tree 0973976619bd4e752a0e825bfe0bb18fa125f575

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    if l1 == l2 && l2 == l3
+      :equilateral
+    elsif
+      l1 == l2 || l2 == l3 || l1 == l3
+      :isosceles
+      elsif
+        l1 != l2 || l2 != l3 || l1 != l3
+        :scalene
+    end
+  end
+
+  def triangle
+    if self.l1 == 0 || self.l2 = 0 || self.l3 = 0 
+      raise TriangleError
+    end
+  end
+
+
+  
+    
+
+  class TriangleError < StandardError
+    def message
+      puts "idk"
+    end
+  end
 end
+# puts 0
